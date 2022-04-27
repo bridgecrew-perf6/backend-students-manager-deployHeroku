abc81b3760d40dede627c71ec6dd6187492fa097
ruanvsrateira <101360876+ruanvsrateira@users.noreply.github.com>
2022-04-27 03:42:36 -0300

    feat: added config for connect bd mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,11 @@
 import app from './app';
+import { connect } from 'mongoose';
 
-app.listen(3333, () => console.log("Servidor Rodando ..."));
+connect("mongodb+srv://ruanvsrateira:Senac123@cluster0.l8sdn.mongodb.net/StudentsDatabase?retryWrites=true&w=majority").then(() => {
+    console.log("MONGOOSE READY")
+    app.emit("DATABASE_ON");
+});
+
+app.on("DATABASE_ON", () => {
+    app.listen(3333, () => console.log("Servidor Rodando ..."));
+});
